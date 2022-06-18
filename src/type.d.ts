a38f659aa406d73671a30a88656999ac5e3f3a1e
Cristóbal Contreras Rubio <crisconru@gmail.com>
2022-06-18 22:12:12 +0200

    creating package types

--- src/type.d.ts
+++
@@ -1,3 +0,0 @@
-import Jimp from 'jimp/*'
-
-export type IMG_SOURCE = string | Object | Buffer | Jimp
---
+++ src/types.d.ts
@@ -0,0 +1,8 @@
+import Jimp from 'jimp/*'
+
+export declare type IMG_SOURCE = string | Object | Buffer | Jimp
+
+export declare interface Size {
+  width: number
+  height: number
+}
