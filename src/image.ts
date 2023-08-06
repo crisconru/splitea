6e1e42586abb19ec3f598ff2911fce0413dbe849
Cristobal Contreras Rubio <crisconru@gmail.com>
2023-08-06 20:16:40 +0200

    extracting unique logic to image file

--- src/image.ts
+++ src/image.ts
@@ -1,7 +1,16 @@
 import Jimp from "jimp"
-import { SpliteaError, throwError } from "../errors"
-import { Image, Size } from "../types"
+import { SpliteaError, ThrowSpliteaError } from "./errors"
+import { Image, Size, SizeSchema } from "./types"
 
+export const readImage = async (image: Image): Promise<[Jimp, Size]> => {
+  try {
+    const img = await Jimp.read(image)
+    const size: Size = SizeSchema.parse({ width: img.bitmap.width, height: img.bitmap.height })
+    return [img, size]
+  } catch (error) {
+    throw ThrowSpliteaError(error, `Error reading image ${image}`)
+  }
+}
 
 export const getSplitImage = (image: Jimp, x: number, y: number, w: number, h: number): Jimp => {
   try {
@@ -11,19 +20,19 @@ export const getSplitImage = (image: Jimp, x: number, y: number, w: number, h: n
     if ((y + h) > height) throw new SpliteaError(`Can't have an image of ${w}x${h}px from (${x}, ${y}) because max y value is ${height - 1}`)
     return image.clone().crop(x, y, w, h)
   } catch (error) {
-    throw throwError(error, 'Problem spliting image')
+    throw ThrowSpliteaError(error, 'Problem spliting image')
   }
 }
 
-export const readImage = async (image: Image): Promise<[Jimp, Size]> => {
-  try {
-    const img = await Jimp.read(image)
-    const size: Size = { width: img.bitmap.width, height: img.bitmap.height }
-    return [img, size]
-  } catch (error) {
-    throw throwError(error, `Error reading image ${image}`)
-  }
+export const getUniqueImages = (imgs: Jimp[]): Jimp[] => {
+  // 0 or 1 image
+  if (imgs.length <= 1) return imgs
+  // Recursion
+  const unique = imgs[0]
+  const arr = imgs.slice(1).filter(elem => !areEqualImages(unique, elem))
+  return [unique, ...getUniqueImages(arr)]
 }
+
 
 // const writeImage = (image: Jimp, path: string, name: string, index: number, extension: string): string => {
 //   const filename = `${name}_${index}_${new Date().getTime()}${extension}`
@@ -41,7 +50,7 @@ export const areEqualImages = (img1: Jimp, img2: Jimp): boolean => {
     }
   } catch (error) {
     console.log(error)
-    throwError(error, 'Error comparing images')
+    ThrowSpliteaError(error, 'Error comparing images')
   }
   return false
 }
