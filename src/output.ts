8b96c512a71d901c1b2671f2821d80eaceb13bf1
Cristobal Contreras Rubio <crisconru@gmail.com>
2023-09-03 18:39:36 +0200

    tested output schemas

--- src/output.ts
+++ src/output.ts
@@ -1,50 +1,10 @@
-import { existsSync, accessSync, constants, mkdirSync } from 'node:fs'
-import { SpliteaError, ThrowSpliteaError } from "./errors"
-import { ExtensionSchema, Image, Output, OutputSchema, StoreSchema } from "./types"
+import { ExtensionSchema, Image, Output, OutputSchema } from "./types"
 import Jimp from 'jimp'
 import { getBufferImages, writeImages } from './image'
 
-const checkPath = (path: string): void => {
-  try {
-    // Check if path not exist, create
-    if (!existsSync(path)) {
-      mkdirSync(path)
-    }
-    // Check if can write
-    accessSync(path, constants.W_OK)
-  } catch (err) {
-    throw ThrowSpliteaError(err, `Problems parsing path ${path}`)
-  }
-}
-
-const invalidWindowsFilename = (name: string): boolean => {
-  // Windows Regex = /^(con|prn|aux|nul|com\d|lpt\d)$/i
-  const windowsReservedNameRegex = /^(con|prn|aux|nul|com\d|lpt\d)$/
-  const invalidWindowsFilename = new RegExp(windowsReservedNameRegex, 'i')
-  return invalidWindowsFilename.test(name)
-}
-
-const invalidUnixFilename = (name: string): boolean => {
-  // All OS except Windows Regex = /[<>:"/\\|?*\u0000-\u001F]/g
-  const filenameReservedRegex = /[<>:"/\\|?*\u0000-\u001F]/
-  const invalidFilename = new RegExp(filenameReservedRegex, 'g')
-  return invalidFilename.test(name)
-}
-
-const checkName = (name: string): void => {
-  // Check OS valid filename
-  if (invalidUnixFilename(name) || invalidWindowsFilename(name)) {
-    throw new SpliteaError(`Invalid filename -> ${name}`)
-  }
-}
 
 export const checkOutput = (output: Output): void => {
-  const { response, store } = OutputSchema.parse(output)
-  if (response === 'path' || store !== undefined) {
-    const { path, name } = StoreSchema.parse(store)
-    checkPath(path)
-    checkName(name)
-  }
+  OutputSchema.parse(output)
 }
 
 export const getOutput = async (images: Jimp[], output: Output): Promise<Image[]> => {
