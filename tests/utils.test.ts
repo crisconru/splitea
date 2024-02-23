89757589844fee4bbe3775c7b61b75dee14095ea
Cristobal Contreras Rubio <crisconru@gmail.com>
2024-02-23 20:15:10 +0100

    tests passed

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,5 +1,5 @@
 import { describe, test, expect } from 'vitest'
-import { isSubmultiple } from '../src/utils'
+import { invalidFilename, isSubmultiple } from '../src/utils'
 
 describe('Test Utils Module', () => {
   test('isSubmultiple', () => {
@@ -9,4 +9,16 @@ describe('Test Utils Module', () => {
     expect(isSubmultiple(10, 4)).toBeFalsy()
   })  
 
+  test('invalidFilename', () => {
+    // INVALID
+    const invalidFiles = ['foo/bar', 'foo\u0000bar', 'foo\u001Fbar', 'foo*bar', 'foo:bar', 'AUX', 'com1', 'foo\\bar']
+    invalidFiles.forEach(file => {
+      expect(invalidFilename(file)).toBeTruthy()
+    })
+    // VALID
+    const validFiles = ['chess.txt', 'chess.jpg', 'chessFolder', 'chessfolder']
+    validFiles.forEach(file => {
+      expect(invalidFilename(file)).toBeFalsy()
+    })
+  })
 })
