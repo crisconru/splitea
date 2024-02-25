48974b87fc63663dc51a10c75bef00bea0f4364f
Cristobal Contreras Rubio <crisconru@gmail.com>
2024-02-25 17:40:14 +0100

    add better error msg if it is a valibot error

--- src/errors.ts
+++ src/errors.ts
@@ -1,3 +1,5 @@
+import { ValiError } from "valibot"
+
 export class SpliteaError extends Error {
   constructor(msg: string) {
     super(msg)
@@ -7,6 +9,11 @@ export class SpliteaError extends Error {
 
 export const ThrowSpliteaError = (error: any, msg: string): SpliteaError => {
   if (error instanceof SpliteaError) { return error }
+  if (error instanceof ValiError) {
+    const err = error.issues[0].message
+    console.error(`Error: ${msg} -> ${err}`)
+    return new SpliteaError(msg + '\n' + err)
+  }
   console.error(error)
   return new SpliteaError(msg + '\n' + String(error))
 }
